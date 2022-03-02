f7d3fa45367a2a6166cb940fe1faac4ce7851105
Obumezi <eziamakaobum@yahoo.co.uk>
2022-03-02 08:56:45 +0100

    Finished Project 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,12 @@ export function Main() {
     return (
         <div className="main-container">
             <h1>Fun Facts About React</h1>
-            <ul>
+            <ul className="list-style">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan walke</li>
                 <li>Has well over 100k starts on Github</li>
                 <li>Is maintained by facebok</li>
-                <li>Powers thousands of en enterprise apps, including mobile apps</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </div>
     )
